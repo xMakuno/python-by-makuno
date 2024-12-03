3cb4d2af2744de342f7965046dad3658630fa6b9
xMakuno <mamv2001@gmail.com>
2024-12-03 11:43:36 -0600

    add: Corto 1 - Introduccion a Python

---
+++ 01/corto.py
@@ -0,0 +1,61 @@
+"""
+Corto 1:
+
+1. Crear un programa que simule dos dados y ver si son.
+    El programa tendra 2 variables (dado1 y dado2) que tendran como valor
+    una entrada numerica dada por el usuario.
+    El programa debera decir:
+        -Si ambos dados tienen valores iguales.
+        -Si el dado1 es mayor que el dado2
+        -Si el dado2 es mayor que el dado1
+
+
+2. Sabiendo que si escribo la siguiente instruccion o comando en python:
+    print("Hola"*3)
+    El programa imprimira (o dara salida) a lo siguiente:
+    HolaHolaHola
+    
+    Haga un programa en python que logre no imprimir NADA.
+    
+    repetecion = ALGUN VALOR NUMERICO
+    print("Hola"*repeticion)
+    Su mensaje es:     # Aqui no deberia aparecer nada
+    
+
+Credito Extra (Opcional):
+
+Crear un programa que tenga una variable llamada 'mensaje' y que
+esta sea aceptada por el usuario.
+El programa luego le pedira al usuario que indique si:
+    -Al mensaje quiere agregarle un '?' al final del mensaje
+    -Al mensaje quiere agregarle un '!' al final del mensaje
+
+
+# Inicio del programa
+Ingrese su mensaje:                             # impresion de consola
+Hola                                            # mensaje ingresado por el usuario
+El mensaje es una pregunta? (Ingrese 0 o 1)     # impresion de consola
+0                                               # mensaje ingresado por el usuario
+El mensaje es una exclamacion? (Ingrese 0 o 1)  # impresion de consola
+1
+Su nuevo mensaje: Hola!?
+# Fin del programa
+
+
+# Inicio del programa
+Ingrese su mensaje:                             # impresion de consola
+Hola                                            # mensaje ingresado por el usuario
+El mensaje es una pregunta? (Ingrese 0 o 1)     # impresion de consola
+0                                               # mensaje ingresado por el usuario
+El mensaje es una exclamacion? (Ingrese 0 o 1)  # impresion de consola
+1
+Su nuevo mensaje: Hola!
+# Fin del programa
+
+
+Sugerencia: 
+Utilizar valores booleanos y operaciones con Strings (multiplicacion o repeticion de Strings)
+para obtener el resultado
+
+
+"""
